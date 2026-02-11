446dc3e77fbccf584406990b905ebc6f16a7f755
Luan Silva <luan.dev.silva@gmail.com>
2026-02-11 01:29:43 -0300

    Add Swagger

--- ProjetoLivros/ProjetoLivros.API/Program.cs
+++ ProjetoLivros/ProjetoLivros.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoLivros.API.Endpoints;
 using ProjetoLivros.Banco;
 using ProjetoLivros.Modelos;
 using System.Data.SqlTypes;
@@ -8,75 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BibliotecaContext>();
 builder.Services.AddTransient<DAL<Colecao>>();
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/Colecoes", ([FromServices] DAL<Colecao> DAL) =>
-{
-    var colecao = DAL.ListaRecuperarPor(c => c.Id > 0);
+app.AddEndpointsColecoes();
 
-    if(colecao == null)
-    {
-        return Results.NotFound();
-    }
-    else
-    {
-        return Results.Ok(colecao);
-    }
-
-});
-
-app.MapGet("/Colecoes/{Nome}", ([FromServices] DAL < Colecao > DAL, string Nome) =>
-{
-    var colecao = DAL.RecuperarPor(c => c.Nome.ToUpper().Equals(Nome.ToUpper()) );
-
-    if (colecao == null)
-    {
-        return Results.NotFound();
-    }
-    else
-    {
-        return Results.Ok(colecao);
-    }
-});
-
-app.MapPost("/Colecoes", ([FromServices] DAL <Colecao> DAL, [FromBody]Colecao colecao) =>
-{
-    DAL.Adicionar(colecao);
-    return Results.Ok();
-});
-
-app.MapDelete("/Colecoes/{id}", ([FromServices] DAL <Colecao> DAL, int id) =>
-{
-    var colecao = DAL.RecuperarPor(c => c.Id == id);
-
-    if(colecao == null)
-    {
-        return Results.NotFound();
-    }
-    else
-    {        
-        DAL.Remover(colecao);
-        return Results.NoContent();
-    }
-});
-
-app.MapPut("/Colecoes/", ([FromServices] DAL<Colecao> DAL, [FromBody] Colecao colecao) => 
-{
-
-    var colecaoAtualiza = DAL.RecuperarPor(c => c.Id == colecao.Id);
-
-    if(colecaoAtualiza == null)
-    {
-        return Results.NotFound();
-    }
-    else
-    {
-        colecaoAtualiza.Nome = colecao.Nome;
-        colecaoAtualiza.Id = colecao.Id;
-        DAL.Atualizar(colecaoAtualiza);
-        return Results.Ok();
-    }
-
-});
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
