d313238b07f70ac69179bc1115a9ca52be5b6777
Luan Silva <luan.dev.silva@gmail.com>
2026-03-07 01:29:14 -0300

    Finalizando introdução a JavaScript

---
+++ ProjetoLivros/JavaScript _TypeScript/Biblioteca.js
@@ -0,0 +1,15 @@
+export class Biblioteca {
+    Livros;
+    Aberta;
+
+    constructor(Aberta){
+        this.Aberta = Aberta;
+    }
+
+    Livros = new Array(
+        "Armagedom 2419 A.D.",
+        "Histórias primordiais",
+        "A canção de Erich Zann",
+        "Deuses exteriores"
+    );
+}
--- ProjetoLivros/JavaScript _TypeScript/MyFirstProgram.js
+++ ProjetoLivros/JavaScript _TypeScript/MyFirstProgram.js
@@ -1,31 +1,29 @@
-console.log("Trabalhado com Variaveis");
+import { Biblioteca } from "./Biblioteca.js";
+import { Usuario } from "./Usuario.js";
 
-const nome = "Luan Silva", altura = 173, peso = 56, idade = 23;
+const biblioteca = new Biblioteca("Aberta");
+const usuario = new Usuario();
 
-console.log("Nome: " + nome + ", Idade: " + idade + ", Altura: " + altura + ", Peso: " + peso);
+usuario.Nome = "Luan Silva";
+usuario.Idade = 23;
+usuario.Rg = "56.656.820.2";
 
-console.log(`Teste de lista de Livros: `);
+console.log("\nNome: " + usuario.Nome + ", Idade: " + usuario.Idade + ", Rg: " + usuario.Rg);
 
-const listaLivros = new Array(
-    "Armagedom 2419 A.D.",
-    "Histórias primordiais",
-    "A canção de Erich Zann",
-    "Deuses exteriores"
-);
+console.log(`Estado da Biblioteca: ${biblioteca.Aberta}\n\n Lista de Livros: `);
 
-console.log(listaLivros);
+console.log(biblioteca.Livros);
 
-listaLivros.push("Os iluminados")
+biblioteca.Livros.push("Os iluminados")
 
-if(listaLivros.find(l => l == "Os iluminados"))
-{
+if (biblioteca.Livros.find(l => l == "Os iluminados")) {
     console.log("Encontrado");
-    console.log(listaLivros);
-    listaLivros.splice(1,2);
+    console.log(biblioteca.Livros);
+    biblioteca.Livros.splice(1, 2);
 }
-else
-{
+else {
     console.log("Não encontrado");
 }
 
-console.log(listaLivros[2]);
+console.log(biblioteca.Livros[2]);
+
