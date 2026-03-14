15ae11fff01f78cf21cb6eba873330cc66b383b6
Luan Silva <luan.dev.silva@gmail.com>
2026-03-14 00:29:19 -0300

    Adicionado footer e mapeamento

--- ProjetoLivros/Angular/Biblioteca/src/app/app.routes.ts
+++ ProjetoLivros/Angular/Biblioteca/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/pages/inicio/inicio';
+import { Sobre } from './components/pages/sobre/sobre';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component : Inicio
+    },
+
+    {
+        path: 'sobre',
+        component: Sobre
+    },
+
+    {
+    path: '**',
+    redirectTo: ''
+    }
+
+];
--- ProjetoLivros/Angular/Biblioteca/src/app/app.ts
+++ ProjetoLivros/Angular/Biblioteca/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Inicio } from "./components/pages/inicio/inicio";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
