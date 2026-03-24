e711e6b8dca0826249c3adcddc13737ad22cd536
Luan Silva <luan.dev.silva@gmail.com>
2026-03-24 00:33:00 -0300

    Tentando arrumar o botão

--- ProjetoLivros/Angular/Biblioteca/src/app/pages/configuracoes/configuracoes.ts
+++ ProjetoLivros/Angular/Biblioteca/src/app/pages/configuracoes/configuracoes.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { PainelExpansivel } from "../../shared/components/painel-expansivel/painel-expansivel";
+import { PainelExpansivelComponent } from '../../shared/components/painel-expansivel/painel-expansivel';
+import { BotaoPainelComponent } from '../../shared/components/botao-painel/botao-painel';
 
 @Component({
   selector: 'app-configuracoes',
-  imports: [PainelExpansivel],
-  templateUrl: './configuracoes.html',
-  styleUrl: './configuracoes.css',
+  standalone: true,
+  imports: [
+    PainelExpansivelComponent,
+    BotaoPainelComponent
+  ],
+  templateUrl: './configuracoes.html'
 })
-export class Configuracoes {}
+export class ConfiguracoesComponent {
+  expandido = false;
+  larguraPainel = 200;
 
-
-
+  togglePainel() {
+    this.expandido = !this.expandido;
+    this.larguraPainel = this.expandido ? 325 : 200;
+  }
+}
