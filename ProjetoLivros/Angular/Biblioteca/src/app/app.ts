193cf1c0594a30a5459aceb6d40ffa4ab5481987
Luan Silva <luan.dev.silva@gmail.com>
2026-03-27 00:54:26 -0300

    Aplicando Accordion

--- ProjetoLivros/Angular/Biblioteca/src/app/app.ts
+++ ProjetoLivros/Angular/Biblioteca/src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/layout/header/header";
 import { Footer } from "./core/layout/footer/footer";
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer, MatExpansionModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
